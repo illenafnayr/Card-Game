2ba254ae5a6a67c745269ab9fb6651ac0da22f11
Ryan Fanelli <illenafnayr@gmail.comgit config --global user.name Ryan>
2020-12-03 14:48:43 -0500

    changed stuff

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ jQuery(() => {
       $.ajax({
         url: `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=52` //draws 52 cards from deck with unique deckID
       }).then((data) => {
-        console.log(data.cards);
-        const cardNum = Math.floor(Math.random() * 52)// creates random number betweeb 0 and 51 to use to choose from array of cards.
-        const $card = $('<img>').attr('src', data.cards[cardNum].image).attr('alt',data.cards[cardNum].code)
-        $('body').append($card)
+        // console.log(data.cards);
+        // const cardNum = Math.floor(Math.random() * 52)// creates random number betweeb 0 and 51 to use to choose from array of cards.
+        // const $card = $('<img>').attr('src', data.cards[cardNum].image).attr('alt',data.cards[cardNum].code)
+        // $('body').append($card)
       },
       () => {
         console.log('error');
@@ -23,8 +23,8 @@ jQuery(() => {
     console.log('error');
   })
 
-  const $cardBack = $('<img>').attr('src', 'https://www.pngkey.com/png/full/349-3492792_card-back.png').attr('alt', 'card back').css({'width': '226px', 'height': '314'})
-  $('body').append($cardBack)
+  // const $cardBack = $('<img>').attr('src', 'https://www.pngkey.com/png/full/349-3492792_card-back.png').attr('alt', 'card back').css({'width': '226px', 'height': '314'})
+  // $('body').append($cardBack)
 
 
 
