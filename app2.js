5edfe68d3ded675ab1e68958b67d4d060c83d816
Ryan Fanelli <illenafnayr@gmail.comgit config --global user.name Ryan>
2020-12-04 07:40:22 -0500

    When cards are appended to page, they are removed from shuffled deck

--- app2.js
+++ app2.js
@@ -12,7 +12,6 @@ jQuery(() => {
     console.log('error');
   })
 
-
   const getDeck = (deckID) => {
     $.ajax({
       url: `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=52` //draws 52 cards from deck with unique deckID
@@ -25,24 +24,32 @@ jQuery(() => {
       console.log('error');
     })
   }
-
+  let shuffledDeck = []
   $('#shuffle').on('click',() => {
-      let shuffledDeck = []
-      for (var i = (deck.cards).length; i > 0; i--) {
+
+      for (let i = (deck.cards).length; i > 0; i--) {
       const cardNum = Math.floor(Math.random() * i)
       shuffledDeck.push(deck.cards.splice(cardNum, 1))
     }
+    console.log(shuffledDeck[0][0]);
 
-    for (var i = 0; i < shuffledDeck.length; i++) {
-      console.log(shuffledDeck[i][0].code);
-      const $card = $('<img>').attr('src', shuffledDeck[i][0].image)
-      $('#userContainer').append($card)
-    }
-
-
-
+    // for (var i = 0; i < shuffledDeck.length; i++) {
+    //   console.log(shuffledDeck[i][0]);
+    //   const $card = $('<img>').attr('src', shuffledDeck[i][0].image)
+    //   $('#userContainer').append($card)
+    // }
 
   })
+
+  $('#cardBack').on('click', () => {
+
+    const $card = $('<img>').attr('src', shuffledDeck[0][0].image)
+    $('#userContainer').append($card)
+    shuffledDeck.splice(0, 1)
+    console.log(shuffledDeck[0][0]);
+    console.log(shuffledDeck.length);
+  })
+
 
   // $('#cardBack').on('click',() => {
   //   console.log(deck.cards);
