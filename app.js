01462eb3f8b08b1902e3ab54e1de280a02d508ac
Ryan Fanelli <illenafnayr@gmail.comgit config --global user.name Ryan>
2020-12-05 19:56:59 -0500

    Can deal to both user and computer, user and computer can discard

--- app.js
+++ app.js
@@ -4,30 +4,25 @@ jQuery(() => {
     url: 'https://deckofcardsapi.com/api/deck/new/'
   }).then(
     (deck) => {
-      // console.log('works');
+      // console.log(deck.deck_id);
       getDeck(deck.deck_id);
   },
   (error) => {
     console.log('error');
   })
-
   const getDeck = (deckID) => {
     $.ajax({
       url: `https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=52` //draws 52 cards from deck with unique deckID
-
     }).then(
       (data) => {
        // creates global deck variable
       deck.cards = data.cards
       console.log(deck, 'initial deck');
-
     },
     () => {
       console.log('error');
     })
   }
-
-
 //Deck Piles:
   let deck = {
     cards: []
@@ -35,7 +30,6 @@ jQuery(() => {
   let discardPile = {
     cards: []
   }
-
 //Functions:
   const shuffle = (array) => {
     // shuffledDeck.cards = []
@@ -58,36 +52,24 @@ jQuery(() => {
     }
     return array;
   }
-  const dealCards = (num) => {
-    $('#userContainer').css('visibility', 'visible')
+  const dealCardsComputer = (num) => {
+    $('#computer').css('visibility', 'visible')
     for (let i = 0; i < num; i++) {
       const $card = $('<img class = "card">').attr('src', deck.cards[0].image).attr('id', deck.cards[0].code)
-      $('#userContainer').append($card)
+      $('#computer').append($card)
+      discardPile.cards.push(deck.cards.splice(0, 1)[0])//since .splice returns array and only splilcing one object use [0] to select that object
+    }
+  }
+  const dealCardsUser = (num) => {
+    $('#user').css('visibility', 'visible')
+    for (let i = 0; i < num; i++) {
+      const $card = $('<img class = "card">').attr('src', deck.cards[0].image).attr('id', deck.cards[0].code)
+      $('#user').append($card)
       discardPile.cards.push(deck.cards.splice(0, 1)[0])//since .splice returns array and only splilcing one object use [0] to select that object
     }
     console.log(deck, 'deck remaining');
   }
-  const addPlayer = () => {
-
-  }
-
-//Event Handlers:
-  $('#shuffle').on('click',() => {
-    $('#cardBack').addClass('cardBackShuffle').delay(1001).queue(() => {
-      $('#cardBack').removeClass('cardBackShuffle').dequeue()
-      })
-    shuffle(deck.cards)
-    console.log(deck, 'deck shuffled')
-  }) // shuffle deck
-  $('#cardBack').on('click', () => {
-    let num = 1
-    dealCards(num)
-  }) // deal individual card to user
-  $('#dealCards').on('click', () => {
-    let num = prompt('How many cards do you want to deal?', 'enter a number')
-    dealCards(num)
-  }) //deal x amount of cards to user
-  $('#userContainer').on('click', (event) => {
+  const discard = (event) => {
     $('#discardPile').css('display', 'flex')
     const $discardPile = $('#discardPileSub')
     console.log(discardPile, 'disccardPile');
@@ -96,21 +78,54 @@ jQuery(() => {
       $('#buttons').append('<button type="button" name="button" id="shuffleDiscard" class="button">Shuffle Discard Pile</button>')
     } else {
       return
-    }
-
-  }) //discard selected card from user container
+    }}
+//Event Handlers
+    // shuffle deck
+  $('#shuffle').on('click',() => {
+    $('#cardBack').addClass('cardBackShuffle').delay(1001).queue(() => {
+      $('#cardBack').removeClass('cardBackShuffle').dequeue()
+      })
+    shuffle(deck.cards)
+    console.log(deck, 'deck shuffled')
+  })
+    // deal individual card to user
+  $('#cardBack').on('click', (event) => {
+    let num = 1
+    dealCardsUser(num)
+  })
+    //deal x amount of cards to user
+  $('#dealCards').on('click', () => {
+    let num = prompt('How many cards do you want to deal?', 'enter a number')
+    dealCardsUser(num)
+    dealCardsComputer(num)
+  })
+    //discard selected card from user container
+  $('#user').on('click','.card', () => {
+    discard(event)
+  })
+    //discard selected card from computer container
+  $('#computer').on('click','.card', () => {
+    discard(event)
+  })
+    //shuffle discard pile and re append shuffled imgs
   $('#buttons').on('click','#shuffleDiscard', () => {
-    // $('#cardBack').addClass('cardBackShuffle').delay(1001).queue(() => {
-    //   $('#cardBack').removeClass('cardBackShuffle').dequeue()
-    //   })
-
     shuffle(discardPile.cards)//shuffle discard array
     $('#discardPileSub').children().remove()//remove imgs from discard pile
     for (let i = 0; i < discardPile.cards.length; i++) { //append shuffled array imgs to discard pile
       const $discardPile = $('#discardPileSub')
-      $discardCard = $('<img>').attr('src', discardPile.cards[i].image).attr('id', discardPile.cards[i].code)
+      $discardCard = $('<img>').attr('src', discardPile.cards[i].image).attr('id', discardPile.cards[i].code).addClass('cardDiscard')
       $discardPile.prepend($discardCard)
     }
-    console.log(discardPile.cards, 'discard shuffled');
-  }) //shuffle discard pile and re append shuffled imgs
+    console.log(discardPile.cards, 'discard shuffled')
+  })
+    //right click to deal top card to computer
+  $('#cardBack').on('contextmenu', (event) => {
+    event.preventDefault()
+    let num = 1
+    dealCardsComputer(num)
+  })
+
+
+
+
 })
